1658ca675b9c6147b9c5ce9a56ab3bbb3b7c464f
gjesionowski <gregory.a.jesionowski@gmail.com>
2021-02-16 15:44:22 -0800

    Adds second converter for lbs to kgS

--- app/src/main/java/com/example/unitcoverter/Converter.java
+++ app/src/main/java/com/example/unitcoverter/Converter.java
@@ -2,6 +2,10 @@ package com.example.unitcoverter;
 
 public class Converter {
     public static float toCelcius(float fah) {
-        return (fah - 32) * 5/9;
+        return (fah - 32) * 5 / 9;
+    }
+
+    public static float toKilograms(float lbs) {
+        return (float) (lbs * 0.45359237);
     }
 }
--- app/src/main/java/com/example/unitcoverter/MainActivity.java
+++ app/src/main/java/com/example/unitcoverter/MainActivity.java
@@ -15,14 +15,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button fbtn = findViewById(R.id.convert_btn);
+        Button lbsbtn = findViewById(R.id.convert_btn2);
 
         EditText finput = findViewById(R.id.fahrenheit_input);
         TextView ctxt = findViewById(R.id.celcius_text);
+        EditText lbsinput = findViewById(R.id.pounds_input);
+        TextView kgtxt = findViewById(R.id.kilograms_text);
+
+
         fbtn.setOnClickListener(v -> {
             if (finput.getText().length() == 0)
                 return;
             double celcius = Converter.toCelcius(Float.parseFloat(finput.getText().toString()));
             ctxt.setText(String.format("%.2f ºC", celcius));
+        });
+
+        lbsbtn.setOnClickListener(v -> {
+            if (lbsinput.getText().length() == 0)
+                return;
+            double kilograms = Converter.toKilograms(Float.parseFloat(lbsinput.getText().toString()));
+            kgtxt.setText(String.format("%.2f kg", kilograms));
         });
     }
 }
